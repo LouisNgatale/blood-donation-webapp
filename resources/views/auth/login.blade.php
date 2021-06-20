2a30d70bdf4b29665e95d6ad25e4351fb3f47b2b
Louis Ngatale <louisngatale@gmail.com>
2021-06-20 21:13:48 +0300

    Authentication pages refurbish

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,60 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-        <div class="container">
-            <a class="navbar-brand" href="{{ url('/') }}">
-                {{ config('app.name', 'Laravel') }}
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <!-- Left Side Of Navbar -->
-                <ul class="navbar-nav mr-auto">
-
-                </ul>
-
-                <!-- Right Side Of Navbar -->
-                <ul class="navbar-nav ml-auto">
-                    <!-- Authentication Links -->
-                    @guest
-                        @if (Route::has('login'))
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                        @endif
-
-                        @if (Route::has('register'))
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                            </li>
-                        @endif
-                    @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }}
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
-                    @endguest
-                </ul>
-            </div>
-        </div>
-    </nav>
-
     <div class="container py-4">
     <div class="row justify-content-center">
         <div class="col-md-5">
@@ -67,6 +13,7 @@
                         </div>
                         <div class="mb-3">
                             <h4 class="text-center" style="color: darkred">BLOOD BANK</h4>
+                            <h6 class="text-center">Login</h6>
                         </div>
                         <div class="form-group row">
                             <div class="col mb-2">
@@ -109,6 +56,11 @@
                                 <button  type="submit" class="btn w-100 btn-primary">
                                     {{ __('Login') }}
                                 </button>
+                            </div>
+                        </div>
+                        <div class="form-group row mt-2 mb-0">
+                            <div class="col">
+                                <a class="btn w-100" href="{{ route('register') }}">{{ __('Register') }}</a>
                             </div>
                         </div>
                     </form>
